cfd447c8c82a8875eeea1214528e218e31922f6c
Thomas Högberg <thomas.hogberg@devies.se>
2024-11-01 16:41:15 +0100

    run script

--- snmp.py
+++ snmp.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import subprocess
 
 
 def check_toner_status():
@@ -8,3 +9,33 @@ def check_toner_status():
 
     assert printer_ip is not None, "PRINTER_IP not configured."
     logging.info(f"Using printer IP {printer_ip}")
+
+    outputs = []
+    for color in [
+        "black",
+        "cyan",
+        "magenta",
+        "yellow",
+        "drum",
+    ]:
+        result = _get_toner_level(printer_ip, color)
+
+        out = _map_output(result)
+        outputs.append(out)
+
+    return outputs
+
+
+def _get_toner_level(ip, color):
+    arguments = [f"-H", ip, "-t", "consumable", "-o", color]
+
+    script_path = "check_snmp_printer"
+    result = subprocess.run(
+        ["bash", script_path] + arguments, capture_output=True, text=True
+    )
+    return result
+
+
+def _map_output(text):
+    logging.info(text)
+    return text
