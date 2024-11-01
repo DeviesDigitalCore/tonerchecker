991fdc4bc253974937ab8799375f771cdce0aa65
Thomas Högberg <thomas.hogberg@devies.se>
2024-11-01 16:55:02 +0100

    post low levels to slack

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
-from snmp import check_toner_status
+import os
+from snmp import TonerStatus, check_toner_status
 import dotenv
 import glob
 from slack import post_to_slack
@@ -11,7 +12,21 @@ def configure():
         dotenv.load_dotenv(env_file)
 
 
+def levels_to_message(levels: list[TonerStatus]):
+    message = "The follow toner levels are low:"
+    for level in levels:
+        message += f"\n{level.color}: {level.level}%"
+
+    return message
+
+
+low_level = int(os.getenv("LOW_LEVEL", 0))
+
 configure()
 
-check_toner_status()
-post_to_slack()
+statuses = check_toner_status()
+low_statuses = [s for s in statuses if s.level <= low_level]
+
+if any(low_statuses):
+    message = levels_to_message(low_statuses)
+    post_to_slack(message)
--- slack.py
+++ slack.py
@@ -4,18 +4,18 @@ import os
 import requests
 
 
-def post_to_slack():
+def post_to_slack(message):
     post_to_slack = os.getenv("SLACK_POST", False)
     if post_to_slack:
-        _post_to_slack()
+        _post_to_slack(message)
     else:
         logging.info("Would post to slack but is configured not to.")
 
 
-def _post_to_slack():
+def _post_to_slack(message):
     slack_url = os.getenv("SLACK_URL")
     assert slack_url is not None, "SLACK_URL not configured"
 
     logging.debug(f"Using {slack_url}")
 
-    requests.post(slack_url, json={"text": "Hello, world."})
+    requests.post(slack_url, json={"text": message})
