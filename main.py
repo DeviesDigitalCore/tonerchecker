77109d4d107730d22df677d69f5b3760b85318a1
Thomas Högberg <thomas.hogberg@devies.se>
2024-11-01 15:41:08 +0100

    load url

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import logging
+import os
+import dotenv
+import glob
 
-logging.basicConfig(level=logging.DEBUG)
-logging.info("Foo")
+
+def configure():
+    logging.basicConfig(level=logging.DEBUG)
+    for env_file in glob.glob(".env*"):
+        dotenv.load_dotenv(env_file)
+
+
+configure()
+
+slack_url = os.getenv("SLACK_URL")
+assert slack_url is not None, "SLACK_URL not configured"
+
+logging.debug(f"Using {slack_url}")
